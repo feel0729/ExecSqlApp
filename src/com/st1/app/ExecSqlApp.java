20f9d837273f6e7991d003325768fc4e9cbc5bba
feel0729 <feel0729@gmail.com>
2022-05-25 14:30:21 +0800

    Wei

--- src/com/st1/app/ExecSqlApp.java
+++ src/com/st1/app/ExecSqlApp.java
@@ -242,6 +242,8 @@ public class ExecSqlApp {
 
     if (!allFiles.isEmpty()) {
       fileList.setListData(allFileNames.split(","));
+    } else {
+      fileList.setListData(new String[0]);
     }
   }
 
